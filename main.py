86007e850deac8862fe4f8736733cb6c7e3e7ad4
anuragrao04 <raoanu2004@gmail.com>
2024-11-06 08:56:54 +0530

    added continuous callout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyglet
 import os
 from datetime import date
+import time
 
 absent_list = []
 for student_file_name in os.listdir("./students"):
@@ -8,16 +9,10 @@ for student_file_name in os.listdir("./students"):
         "./students/" + student_file_name, streaming=False)
     student_name = student_file_name[:-4].replace("_", " ")
 
-    while (True):
-        os.system('clear')
-        print(f"Is {student_name} present? ((r)epeat/(p)resent/(a)bsent)")
-        audio.play()
-        response = input().lower()
-        if response == 'a':
-            absent_list.append(student_name)
-            break
-        elif response == 'p':
-            break
+    os.system('clear')
+    print(f"Is {student_name} present?")
+    audio.play()
+    time.sleep(3)
 
 print("Absent Students: ")
 for student in absent_list:
